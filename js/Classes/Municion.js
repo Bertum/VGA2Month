a62c6732d1786f54ad4fa20a88f890ac6b4626de
Miguel <magudob@gmail.com>
2018-10-27 12:32:54 +0200

    code cleaned

--- js/Classes/Municion.js
+++
@@ -1,9 +0,0 @@
-//Declaracion objeto Municion
-function Municion(src, munX, munY, damage, munV) {
-    this.sprite = new Image();
-    this.sprite.src = src;
-    this.munX = munX;
-    this.munY = munY;
-    this.munV = munV;
-    this.damage = damage;
-}
