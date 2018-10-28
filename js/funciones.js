e7ba1c6491ddc5541922fefe1f784b73f86a0479
Carlos Megia <cmttar@gmail.com>
2018-10-28 21:48:57 +0100

    arreglos toque
    duración mano desactivada popr el momento, a partir de la primera mariposa no deja golpear más

--- js/funciones.js
+++ js/funciones.js
@@ -90,39 +90,36 @@ function toque(posx, posy) {
     for (var i = 0; i < numFly; i++) {
         if (posx > arrFly[i].posX && posx < arrFly[i].posX + arrFly[i].anchura && posy > arrFly[i].posY && posy < arrFly[i].posY + arrFly[i].altura) {
             console.log("le has dado a una mosca");
-            //if (hitDone == 0) {
             arrayManos.push(new Hand(hitImg, posx - hitImg.width / 2, posy - hitImg.height / 2));
             gameContext.drawImage(arrayManos[arrayManos.length - 1].sprite, arrayManos[arrayManos.length - 1].posX, arrayManos[arrayManos.length - 1].posY);
             //hitDone = 1;
-            //}
-            //gameContext.drawImage("img/bloodsplat2.png", arrFly[i].posX, arrFly[i].posY);
-
+            /*flyImg.src = "img/fly_small.png"
+            gameContext.drawImage(arrFly[i].sprite, arrFly[i].posX, arrFly[i].posY);
+            flyImg.src = "img/bloodsplat2.png"*/
+            arrFly.splice(i, 1);
         }
         else {
-            //if (hitDone == 0) {
             arrayManos.push(new Hand(handImg, posx - handImg.width / 2, posy - handImg.height / 2));
             gameContext.drawImage(arrayManos[arrayManos.length - 1].sprite, arrayManos[arrayManos.length - 1].posX, arrayManos[arrayManos.length - 1].posY);
             //hitDone = 1;
-            //}
         }
     }
     for (var i = 0; i < numButterfly; i++) {
         if (posx > arrButterfly[i].posX && posx < arrButterfly[i].posX + arrButterfly[i].anchura && posy > arrButterfly[i].posY && posy < arrButterfly[i].posY + arrButterfly[i].altura) {
             console.log("le has dado a una mariposa");
-            //if (hitDone == 0) {
             arrayManos.push(new Hand(hitImg, posx - hitImg.width / 2, posy - hitImg.height / 2));
             gameContext.drawImage(arrayManos[arrayManos.length - 1].sprite, arrayManos[arrayManos.length - 1].posX, arrayManos[arrayManos.length - 1].posY);
             //hitDone = 1;
-            // }
             cuentaMariposas++;
-            //butterflyImg.src = "img/bloodsplat2.png"
+            /*butterflyImg.src = "img/bloodsplat2.png"
+            gameContext.drawImage(arrButterfly[i].sprite, arrButterfly[i].posX, arrButterfly[i].posY);
+            butterflyImg.src = "img/butterfly.png"*/
+            arrButterfly.splice(i, 1);
         }
         else {
-            // if (hitDone == 0) {
             arrayManos.push(new Hand(handImg, posx - handImg.width / 2, posy - handImg.height / 2));
             gameContext.drawImage(arrayManos[arrayManos.length - 1].sprite, arrayManos[arrayManos.length - 1].posX, arrayManos[arrayManos.length - 1].posY);
             //hitDone = 1;
-            // }
         }
     }
 }
@@ -131,11 +128,11 @@ function duracionMano() {
     if (hitDone == 1) {
         console.log("entro aqui");
         gameContext.drawImage(arrayManos[arrayManos.length - 1].sprite, arrayManos[arrayManos.length - 1].posX, arrayManos[arrayManos.length - 1].posY);
-        if (hitCounter == 5) {
+        if (hitCount == 50) {
             arrayManos.splice(arrayManos.length - 1, 1);
             hitDone = 0;
         }
-        else { hitCounter++; }
+        else { hitCount++; }
     }
 }
 
